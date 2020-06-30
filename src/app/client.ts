9feda14308005fb9acd65b0712f04ed2bf5de148
Eduardo Jirón <emjironal@gmail.com>
2020-06-29 23:45:55 -0600

    [new] Client interface

---
+++ src/app/client.ts
@@ -0,0 +1,13 @@
+export interface Client
+{
+    id: string,
+    companyName: string,
+    contactName: string,
+    contactTitle: string,
+    address: string,
+    city: string,
+    postalCode: number,
+    country: string,
+    phone: string,
+    fax: string
+}
