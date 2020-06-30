6bdf738cbd712ddba0e5c653459ebc7b5f8f08f6
Eduardo Jirón <emjironal@gmail.com>
2020-06-29 23:46:50 -0600

    [updated] Components changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,39 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Angular Material
+import { MatCardModule } from '@angular/material/card'
+import { MatListModule } from '@angular/material/list'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon'
+
+import { ClientinfoComponent } from './clientinfo/clientinfo.component';
+import { ClientlistComponent } from './clientlist/clientlist.component';
+import { ClientdetailComponent } from './clientdetail/clientdetail.component';
+import { OrderdetailComponent } from './orderdetail/orderdetail.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientinfoComponent,
+    ClientlistComponent,
+    ClientdetailComponent,
+    OrderdetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
